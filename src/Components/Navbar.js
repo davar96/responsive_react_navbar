60eaf3c17f8878d1c0a1da6867392601578d4f7e
Davar96 <david.arustam96@gmail.com>
2023-05-03 17:58:04 +0200

    done the styling and responsivnesse

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Link } from "react-router-dom";
 import "./navbar.css";
+import { FaBars } from "react-icons/fa";
+import { ImCross } from "react-icons/im";
 
 const Navbar = () => {
+  const [Mobile, setMobile] = useState(true);
+
   return (
     <nav className="navbar">
       <h3 className="logo">Logo</h3>
 
-      <ul className="nav-links">
+      <ul
+        className={Mobile ? "nav-links-mobile" : "nav-links"}
+        onClick={() => setMobile(false)}
+      >
         <Link to="/">
           <li>Home</li>
         </Link>
@@ -25,6 +32,9 @@ const Navbar = () => {
           <li>Contact</li>
         </Link>
       </ul>
+      <button className="mobile-menu-icon" onClick={() => setMobile(!Mobile)}>
+        {Mobile ? <ImCross></ImCross> : <FaBars></FaBars>}
+      </button>
     </nav>
   );
 };
